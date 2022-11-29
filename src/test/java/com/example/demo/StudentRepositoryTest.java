241b58b42f218870bbbb9ededca5aed48efdb96c
ch <mkmsacc@outlook.com>
2022-11-29 11:49:48 +0700

    Refactor test

--- src/test/java/com/example/demo/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/StudentRepositoryTest.java
@@ -4,15 +4,30 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
-@DataMongoTest
-@TestPropertySource(properties = "spring.mongodb.embedded.version=4.0.21")
+
+@SpringBootTest
+@Testcontainers
 class StudentRepositoryTest {
+
+    @Container
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:5.0.14");
+
+    @DynamicPropertySource
+    static void mongoProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getConnectionString);
+        registry.add("spring.data.mongodb.database", () -> "database_name");
+    }
+
     @Autowired
     private StudentRepository studentRepository;
 
