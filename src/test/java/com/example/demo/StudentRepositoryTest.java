55f53a2129b5400b262cd4e7a2f3a336cd017183
ch <mkmsacc@outlook.com>
2023-01-23 10:34:56 +0700

    Optimized import

--- src/test/java/com/example/demo/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/StudentRepositoryTest.java
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
